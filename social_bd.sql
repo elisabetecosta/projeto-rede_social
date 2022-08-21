7705da73beeb128c272bf1d37938329553c5a6c3
Wolferware <106547798+Wolferware@users.noreply.github.com>
2022-08-21 23:04:46 +0100

    BD V.1.2
    Versão 1.2 da Base de Dados em SQL
    Tabelas adicionadas: t_perfils, t_gotos, t_favs, t_com1, t_com2
    Alguns campos foram optimizados

--- social_bd.sql
+++ social_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04-Ago-2022 às 22:06
+-- Tempo de geração: 22-Ago-2022 às 00:02
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 7.4.29
 
@@ -26,15 +26,77 @@ USE `social_bd`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `t_comentarios`
+-- Estrutura da tabela `t_com1`
 --
 
-CREATE TABLE `t_comentarios` (
-  `id_comment` double NOT NULL,
-  `id_user` double NOT NULL,
+CREATE TABLE `t_com1` (
+  `id_com1` double NOT NULL,
+  `id_autor` double NOT NULL,
   `id_post` double NOT NULL,
-  `texto_comment` varchar(255) NOT NULL,
-  `apagado_c` int(11) NOT NULL DEFAULT 0
+  `texto_com1` varchar(255) NOT NULL,
+  `estado_com1` int(11) NOT NULL DEFAULT 0,
+  `data_com1` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `t_com2`
+--
+
+CREATE TABLE `t_com2` (
+  `id_com2` double NOT NULL,
+  `id_autor` double NOT NULL,
+  `id_com1` double NOT NULL,
+  `texto_com2` varchar(255) NOT NULL,
+  `estado_com2` int(11) NOT NULL DEFAULT 0,
+  `data_com2` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `t_favs`
+--
+
+CREATE TABLE `t_favs` (
+  `id_fav` double NOT NULL,
+  `id_favoritador` double NOT NULL,
+  `id_post` double NOT NULL,
+  `estado_fav` tinyint(1) NOT NULL DEFAULT 0,
+  `data_fav` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `t_fotos`
+--
+
+CREATE TABLE `t_fotos` (
+  `id_foto` double NOT NULL,
+  `id_post` double NOT NULL,
+  `imagem` varchar(255) NOT NULL,
+  `estado_img` int(11) NOT NULL DEFAULT 0,
+  `data_pub` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `t_perfis`
+--
+
+CREATE TABLE `t_perfis` (
+  `id_perfil` double NOT NULL,
+  `id_user` double NOT NULL,
+  `nickname` varchar(45) NOT NULL,
+  `avatar_img` varchar(255) NOT NULL,
+  `capa_img` varchar(255) NOT NULL,
+  `genero` char(45) NOT NULL,
+  `data_nasc` date NOT NULL,
+  `perfil_desc` varchar(255) NOT NULL,
+  `data_updated` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -47,8 +109,22 @@ CREATE TABLE `t_posts` (
   `id_post` double NOT NULL,
   `id_user` double NOT NULL,
   `texto` varchar(255) NOT NULL,
-  `foto_p` varchar(300) DEFAULT NULL,
-  `apagado_p` int(11) NOT NULL DEFAULT 0
+  `id_foto` double DEFAULT NULL,
+  `estado_p` int(11) NOT NULL DEFAULT 0,
+  `privado` int(11) NOT NULL DEFAULT 0,
+  `data_pub` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `t_privado`
+--
+
+CREATE TABLE `t_privado` (
+  `id_privado` double NOT NULL,
+  `id_post` double NOT NULL,
+  `id_user_autorizado` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -59,13 +135,13 @@ CREATE TABLE `t_posts` (
 
 CREATE TABLE `t_users` (
   `id_user` double NOT NULL,
-  `nick` varchar(15) NOT NULL,
+  `handle_user` varchar(15) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `data_nasc` varchar(10) NOT NULL,
-  `foto_perfil` varchar(300) NOT NULL,
-  `foto_capa` varchar(300) NOT NULL,
-  `apagado_u` int(11) NOT NULL DEFAULT 0
+  `estado_user` int(11) NOT NULL DEFAULT 0,
+  `data_regis` datetime NOT NULL,
+  `tipo_user` int(11) NOT NULL DEFAULT 0,
+  `obs_mod` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -73,16 +149,56 @@ CREATE TABLE `t_users` (
 --
 
 --
--- Índices para tabela `t_comentarios`
+-- Índices para tabela `t_com1`
 --
-ALTER TABLE `t_comentarios`
-  ADD PRIMARY KEY (`id_comment`);
+ALTER TABLE `t_com1`
+  ADD PRIMARY KEY (`id_com1`),
+  ADD KEY `id_autor` (`id_autor`),
+  ADD KEY `id_post` (`id_post`);
+
+--
+-- Índices para tabela `t_com2`
+--
+ALTER TABLE `t_com2`
+  ADD PRIMARY KEY (`id_com2`),
+  ADD KEY `id_autor` (`id_autor`),
+  ADD KEY `id_com1` (`id_com1`);
+
+--
+-- Índices para tabela `t_favs`
+--
+ALTER TABLE `t_favs`
+  ADD PRIMARY KEY (`id_fav`),
+  ADD KEY `id_favoritador` (`id_favoritador`),
+  ADD KEY `id_post` (`id_post`);
+
+--
+-- Índices para tabela `t_fotos`
+--
+ALTER TABLE `t_fotos`
+  ADD PRIMARY KEY (`id_foto`),
+  ADD KEY `id_post` (`id_post`);
+
+--
+-- Índices para tabela `t_perfis`
+--
+ALTER TABLE `t_perfis`
+  ADD PRIMARY KEY (`id_perfil`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Índices para tabela `t_posts`
 --
 ALTER TABLE `t_posts`
-  ADD PRIMARY KEY (`id_post`);
+  ADD PRIMARY KEY (`id_post`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_foto` (`id_foto`);
+
+--
+-- Índices para tabela `t_privado`
+--
+ALTER TABLE `t_privado`
+  ADD PRIMARY KEY (`id_privado`);
 
 --
 -- Índices para tabela `t_users`
@@ -95,10 +211,28 @@ ALTER TABLE `t_users`
 --
 
 --
--- AUTO_INCREMENT de tabela `t_comentarios`
+-- AUTO_INCREMENT de tabela `t_com1`
 --
-ALTER TABLE `t_comentarios`
-  MODIFY `id_comment` double NOT NULL AUTO_INCREMENT;
+ALTER TABLE `t_com1`
+  MODIFY `id_com1` double NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `t_com2`
+--
+ALTER TABLE `t_com2`
+  MODIFY `id_com2` double NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `t_favs`
+--
+ALTER TABLE `t_favs`
+  MODIFY `id_fav` double NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `t_fotos`
+--
+ALTER TABLE `t_fotos`
+  MODIFY `id_foto` double NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `t_posts`
@@ -107,10 +241,56 @@ ALTER TABLE `t_posts`
   MODIFY `id_post` double NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `t_privado`
+--
+ALTER TABLE `t_privado`
+  MODIFY `id_privado` double NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `t_users`
 --
 ALTER TABLE `t_users`
   MODIFY `id_user` double NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `t_com1`
+--
+ALTER TABLE `t_com1`
+  ADD CONSTRAINT `t_com1_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `t_posts` (`id_post`);
+
+--
+-- Limitadores para a tabela `t_com2`
+--
+ALTER TABLE `t_com2`
+  ADD CONSTRAINT `t_com2_ibfk_1` FOREIGN KEY (`id_com1`) REFERENCES `t_com1` (`id_com1`);
+
+--
+-- Limitadores para a tabela `t_favs`
+--
+ALTER TABLE `t_favs`
+  ADD CONSTRAINT `t_favs_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `t_posts` (`id_post`);
+
+--
+-- Limitadores para a tabela `t_fotos`
+--
+ALTER TABLE `t_fotos`
+  ADD CONSTRAINT `t_fotos_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `t_posts` (`id_post`);
+
+--
+-- Limitadores para a tabela `t_perfis`
+--
+ALTER TABLE `t_perfis`
+  ADD CONSTRAINT `t_perfis_ibfk_1` FOREIGN KEY (`id_perfil`) REFERENCES `t_users` (`id_user`);
+
+--
+-- Limitadores para a tabela `t_posts`
+--
+ALTER TABLE `t_posts`
+  ADD CONSTRAINT `t_posts_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `t_users` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
