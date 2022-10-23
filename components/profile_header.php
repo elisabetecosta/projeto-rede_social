339304e1dce3b53693a4d16a37725bb65dd28692
Ana <106547798+Wolferware@users.noreply.github.com>
2022-10-23 22:55:18 +0100

    Add files via upload

--- components/profile_header.php
+++ components/profile_header.php
@@ -11,14 +11,14 @@
                 <!-- Nome do Utilizador -->
                 <h2><?php echo $sessionName; ?></a></h2>
                 <!-- @handle do utilizador -->
-                <h4><?php echo $sessionHandle; ?></h4>
+                <h4>@<?php echo $sessionHandle; ?></h4>
             </span>
             <span class="profile-stats">
             <!-- Estatísticas do utilizador: número de posts, número de favoritos, número de pessoas que segue, número de seguidores -->
-                <span class="stats-posts"><h3>Publicações</h3> <a href="#"><?php echo $posts; ?></a></span>
-                <span class="stats-faves"><h3>Favoritos</h3> <a href="#"><?php echo $faves; ?></a></span>
-                <span class="stats-following"><h3>Seguindo</h3> <a href="#"><?php echo $following; ?></a></span>
-                <span class="stats-followers"><h3>Seguidores</h3> <a href="#"><?php echo $followers; ?></a></span>
+                <span class="stats-posts"><h3>Publicações</h3> <a href="../server/posts.php"><?php echo $posts; ?></a></span>
+                <span class="stats-faves"><h3>Favoritos</h3> <a href="../server/favorites.php"><?php echo $faves; ?></a></span>
+                <span class="stats-following"><h3>Seguindo</h3> <a href="../server/following.php"><?php echo $following; ?></a></span>
+                <span class="stats-followers"><h3>Seguidores</h3> <a href="../server/followers.php"><?php echo $followers; ?></a></span>
             </span>
             <span class="profile-btn-container">
                 <button class="btn">Editar</button>
