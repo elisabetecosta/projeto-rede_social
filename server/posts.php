89f70ed0c8603534dc430a0b6d9d1541540f262e
Ana <106547798+Wolferware@users.noreply.github.com>
2022-10-28 01:48:32 +0100

    Atualizado para ficar mais modular

--- server/posts.php
+++ server/posts.php
@@ -1,7 +1,8 @@
 <?php
+    
     include 'session.php';
-    include '../components/navbar.php';    
     include '../components/head.php';
+    include '../components/navbar.php';    
     include '../components/profile_header.php';
     include '../components/generate_posts.php'
 ?>
--- server/session.php
+++ server/session.php
@@ -5,121 +5,135 @@ include 'includes/validate.php';
 //Estabelece a conexão com a base de dados
 include_once 'includes/connect_db.php';
 
-//============== PARA INICIAR A SESSÃO DO UTILIZADOR =================
+//============== FUNÇÕES COM QUERIES PARA IR BUSCAR OS DADOS =================
+//Função que recebe um ID de utilizador e devolve um array de strings: handle, name, avatar, cover, title, desc
+    function get_user_data($uid){
+        require 'includes/connect_db.php';
+        $a = array();
+        $profile = $connection->prepare('SELECT users.handle, profiles.name, profiles.avatar,
+                                                profiles.cover, profiles.title, profiles.desc
+                                            FROM users
+                                            JOIN profiles
+                                            ON users.user_id = profiles.user_id
+                                            WHERE users.user_id = :uid');
+        $profile->bindParam(':uid', $uid);
+        $profile->execute();
+        $data = $profile->fetch(PDO::FETCH_ASSOC);
 
-//Acede aos dados do utilizador com sessão iniciada
-$getProfile = $connection->prepare('SELECT users.handle, profiles.name, profiles.avatar, profiles.cover, profiles.title, profiles.desc
-                                    FROM users
-                                    JOIN profiles
-                                    ON users.user_id = profiles.user_id
-                                    WHERE users.user_id = :uid');
-$getProfile->bindParam(':uid', $_SESSION['user_id']);
-$getProfile->execute();
-$session = $getProfile->fetch(PDO::FETCH_ASSOC);
-    /*A variável $userData contém:
-                                $session['user_id'] = ID do utilizador
-                                $session['handle'] = @username do utilizador
-                                $session['name'] = Nome escolhido pelo do utilizador
-                                $session['avatar'] = avatar do utilizador (avatar.png)
-                                $session['cover'] = capa do utilizador (header_photo.png)
-*/ 
+        if(!empty($data)) {
+                $a['handle'] = $data['handle']; // Handle (sem arroba @)
+                $a['name'] = $data['name'];     // Nome do utilizador
+                $a['avatar'] = $data['avatar']; // Avatar (avatar.png)
+                $a['cover'] = $data['cover'];   // Capa de perfil (cover.png)
+                $a['title'] = $data['title'];   // Título da Descrição ("Sobre mim")
+                $a['desc'] = $data['desc'];     // Texto da Descrição 
+        }
+        return $a;
+    }
 
-//============== LINKS DA BARRA DE NAVEGAÇÃO SUPERIOR =================
-//Gera a barra de navegação superior [navbar.php] com os dados da variável $userData e com os seguintes links:
-$homeURL = "main.php";              //link do logotipo XATA
-$profileURL = "../server/posts.php";        //link do avatar do user (avatar pequenino)
-$notifsURL = "notifications.php";   //link do icone de notificações (sineta)
-$configURL = "config.php";          //link do ícone de configurações (roda dentada)
-$logoutURL = "logout.php";          //link do botão de logout
+//Função que recebe um ID de utilizador e devolve um array com o número de: posts, favoritos, seguindo e seguidores
+    function get_user_stats($uid){
+        require 'includes/connect_db.php';
+        $a = array();
+        //Query que acede ao número total de Publicações deste utilizador
+        $countPosts = $connection->prepare('SELECT COUNT(post_id)
+                                            AS posts
+                                            FROM posts
+                                            WHERE user_id = :uid');
+        $countPosts->bindParam(':uid', $uid);
+        $countPosts->execute();
+        $numPosts = $countPosts->fetch(PDO::FETCH_ASSOC);
 
+        //Query que acede ao número total de Favoritos deste utilizador
+        $countFaves = $connection->prepare('SELECT COUNT(fav_id)
+                                            AS faves
+                                            FROM favs
+                                            WHERE user_id = :uid AND NOT status = 0' ); //Se o utilizador se "arrepender" de favoritar algo, o status passa a 0, logo temos de exclui-los do contador (sem eliminar o registo em si mesmo)
+        $countFaves->bindParam(':uid', $uid);
+        $countFaves->execute();
+        $numFaves = $countFaves->fetch(PDO::FETCH_ASSOC);
 
+        //Query que acede ao número total contas que este utilizador segue
+        $countFollowing = $connection->prepare('SELECT COUNT(follow_id)
+                                            AS following
+                                            FROM follows
+                                            WHERE follower_id = :uid');
+        $countFollowing->bindParam(':uid', $uid);
+        $countFollowing->execute();
+        $numFollowing = $countFollowing->fetch(PDO::FETCH_ASSOC);
 
-//Tenho de fazer uma função de maneira a que as variáveis dos componentes inicializem em função do handle
+        //Query que acede ao número total de contas que seguem este utilizador
+        $countFollowers = $connection->prepare('SELECT COUNT(follow_id) AS followers FROM follows WHERE followed_id = :uid');
+        $countFollowers->bindParam(':uid', $uid);
+        $countFollowers->execute();
+        $numFollowers = $countFollowers->fetch(PDO::FETCH_ASSOC);
+        
+        if(!empty($numPosts) && !empty($numFaves) &&
+           !empty($numFollowing) && !empty($numFollowers)) {
+            $a['posts'] = $numPosts['posts'];
+            $a['faves'] = $numFaves['faves'];
+            $a['following'] = $numFollowing['following'];
+            $a['followers'] = $numFollowers['followers'];
+        }
 
-//============== O PERFIL DO UTILIZADOR COMEÇA AQUI =================
+        return $a;
+    }
 
-    //Cria a header do perfil do utilizador (Capa e Avatar)
-    $handle = $session['handle'];
-    $cover = $session['cover'];
-    $avatar = $session['avatar'];
-    $name =  $session['name'];
+//Função que recebe um ID de utilizador e devolve um array com a galeria do utilizador
+    function get_user_gallery($uid){
+        require 'includes/connect_db.php';
+        $a = array();
+        //Carrega apenas as últimas 4 imagens publicadas por este utilizador (usamos a data de publicação/actualização da tabela posts)
+        $getMedia = $connection->prepare("SELECT media.content, posts.post_id
+                                          FROM media
+                                          JOIN posts
+                                          ON media.post_id=posts.post_id
+                                          WHERE posts.user_id = :uid AND posts.status=0 AND media.status=0
+                                          ORDER BY media.content ASC LIMIT 4");
+        $getMedia->bindParam(':uid', $uid);
+        $getMedia->execute();
+        $displayMedia = $getMedia->fetchAll(PDO::FETCH_ASSOC);
 
-    //Query que acede ao número total de Publicações deste utilizador
-    $getPostCount = $connection->prepare('SELECT COUNT(post_id) AS posts FROM posts WHERE user_id = :uid');
-    $getPostCount->bindParam(':uid', $_SESSION['user_id']);
-    $getPostCount->execute();
-    $counterPosts = $getPostCount->fetch(PDO::FETCH_ASSOC);
+        if(!empty($displayMedia)){
+            $a = $displayMedia;
+        }
+        return $a;
+    }
 
-    //Query que acede ao número total de Favoritos deste utilizador
-    $getFaveCount = $connection->prepare('SELECT COUNT(fav_id) AS faves FROM favs WHERE user_id = :uid AND NOT status=0' ); //Se o utilizador se "arrepender" de favoritar algo, o status passa a 0, logo temos de exclui-los do contador (sem eliminar o registo em si mesmo)
-    $getFaveCount->bindParam(':uid', $_SESSION['user_id']);
-    $getFaveCount->execute();
-    $counterFaves = $getFaveCount->fetch(PDO::FETCH_ASSOC);
+//Função que recebe um ID de utilizador e devolve um array com os seus 10 posts mais recentes
+    function get_user_posts($uid){
+        require 'includes/connect_db.php';
+        $getPosts =  $connection->prepare("SELECT text, date
+                                           FROM posts
+                                           WHERE user_id = :uid AND status = 0 
+                                           ORDER BY date DESC LIMIT 10");
+        $getPosts->bindParam(':uid', $uid);
+        $getPosts->execute();
+        $displayPosts = $getPosts->fetchAll(PDO::FETCH_ASSOC);
+        if(!empty($displayPosts)){
+            $a = $displayPosts;
+        }
+        return $a;
+    }
 
-    //Query que acede ao número total contas que este utilizador segue
-    $getFollowingCount = $connection->prepare('SELECT COUNT(follow_id) AS following FROM follows WHERE follower_id = :uid');
-    $getFollowingCount->bindParam(':uid', $_SESSION['user_id']);
-    $getFollowingCount->execute();
-    $counterFollowing = $getFollowingCount->fetch(PDO::FETCH_ASSOC);
-
-    //Query que acede ao número total de contas que seguem este utilizador
-    $getFollowersCount = $connection->prepare('SELECT COUNT(follow_id) AS followers FROM follows WHERE followed_id = :uid');
-    $getFollowersCount->bindParam(':uid', $_SESSION['user_id']);
-    $getFollowersCount->execute();
-    $counterFollowers = $getFollowersCount->fetch(PDO::FETCH_ASSOC);
-
-    //Mostra as estatísticas deste utilizador 
-    $posts = $counterPosts['posts'];                //Número de publicações
-    $faves = $counterFaves['faves'];                //Número de favoritos
-    $following = $counterFollowing['following'];    //Número de users que segue
-    $followers = $counterFollowers['followers'];    //Número de seguidores
-
-
-//============== O CONTEÚDO ALTERNA ENTRE PUBLICAÇÕES, FAVORITOS, SEGUINDO E SEGUIDORES COM UM CLIQUE NAS STATS  =================
-
-    //==== Caixa de apresentação "Sobre mim"
-    $desc_title = $session['title'];     // "Sobre mim" ou o título personalizado pelo utilizador
-    $description = $session['desc'];     // Descrição do perfil escrita pelo utilizador
-
-    //==== Galeria de imagens
-    //Carrega apenas as últimas 4 imagens publicadas por este utilizador (usamos a data de publicação/actualização da tabela posts)
-    $getMedia = $connection->prepare("SELECT media.content, posts.post_id
-                                    FROM media
-                                    JOIN posts
-                                    ON media.post_id=posts.post_id
-                                    WHERE posts.user_id = :uid AND posts.status=0 AND media.status=0
-                                    ORDER BY media.content ASC LIMIT 4");
-    $getMedia->bindParam(':uid', $_SESSION['user_id']);
-    $getMedia->execute();
-    $displayMedia = $getMedia->fetchAll(PDO::FETCH_ASSOC);
-    $gallerytitle = "Galeria";
-
-    $getPosts =  $connection->prepare("SELECT text, date
-                                    FROM posts
-                                    WHERE user_id = :uid AND status = 0 
-                                    ORDER BY date DESC LIMIT 10");
-    $getPosts->bindParam(':uid', $_SESSION['user_id']);
-    $getPosts->execute();
-    $displayPosts = $getPosts->fetchAll(PDO::FETCH_ASSOC);
-
-    // Função que converte a datahora armazenada em tempo relativo (exemplo: "há 5 segs")
+//Função que recebe a datahora armazenada no sistema e devolve uma string com o tempo relativo (exemplo: "há 5 segs")
     function time_elapsed_string($datetime, $full = false) {
         $now = new DateTime;
         $ago = new DateTime($datetime);
         $diff = $now->diff($ago);
-    
+        
         $diff->w = floor($diff->d / 7);
         $diff->d -= $diff->w * 7;
-    
+        
         $string = array(
-            'y' => 'ano',
-            'm' => 'mês',
-            'w' => 'semana',
-            'd' => 'dia',
-            'h' => 'hora',
-            'i' => 'min',
-            's' => 'seg',
-        );
+                'y' => 'ano',
+                'm' => 'mês',
+                'w' => 'semana',
+                'd' => 'dia',
+                'h' => 'hora',
+                'i' => 'min',
+                's' => 'seg',
+            );
         foreach ($string as $k => &$v) {
             if ($diff->$k) {
                 $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
@@ -127,10 +141,49 @@ $logoutURL = "logout.php";          //link do botão de logout
                 unset($string[$k]);
             }
         }
-    
+        
         if (!$full) $string = array_slice($string, 0, 1);
         return $string ? implode(', ', $string) : 'agora mesmo';
     }
-    ?>
+
+//
+//============== O PERFIL DO UTILIZADOR COMEÇA AQUI =================
+
+//Links da barra de navegação superior [navbar.php]:
+$homeURL = "main.php";                          //link do logotipo XATA
+$profileURL = "../server/posts.php";            //link do avatar do user (avatar pequenino)
+$notifsURL = "notifications.php";               //link do icone de notificações (sineta)
+$configURL = "config.php";                      //link do ícone de configurações (roda dentada)
+$logoutURL = "logout.php";                      //link do botão de logout
+
+//Variáveis do Perfil do Utilizador [profile_header.php]:
+$session = get_user_data($_SESSION['user_id']);   //Recebe um array com os índices:
+$handle = $session['handle'];                   // handle
+$cover = $session['cover'];                     // cover (capa)
+$avatar = $session['avatar'];                   // avatar (avatar.png)
+$name = $session['name'];                       // name (nome do utilizador)
+
+//Configurações do cabeçalho do HTML [head.php]
+$language = "pt-PT";
+$charset = "UTF-8";
+$metaTags = "xata, perfil, rede social";
+$title = "$name (@$handle)";                    //Título da página = Nome do utilizador (@handle)
+
+//Estatísticas do utilizador [profile_header.php]
+$stats = get_user_stats($_SESSION['user_id']);    //Recebe um array com os índices:
+$posts = $stats['posts'];                       // posts (Número de publicações)
+$faves = $stats['faves'];                       // faves (Número de favoritos)
+$following = $stats['following'];               // following (Número de users que seguem este user)
+$followers = $stats['followers'];               // followers (Número de seguidores)
+
+//Variáveis da coluna lateral esquerda da página de publicações do utilizador [posts.php]
+$user_title = $session['title'];
+$user_desc = $session['desc'];
+$gallerytitle = "Galeria";
+$displayMedia = get_user_gallery($_SESSION['user_id']);
+$displayPosts = get_user_posts($_SESSION['user_id']);
+//
+
+?>
 </body>
 </html>
