2f1b9f3ee8cdc1f8c0d4c343216961e3545aee19
Rayana <anapaula.aka@gmail.com>
2022-11-09 16:49:48 +0000

    Comments added

--- server/followers.php
+++ server/followers.php
@@ -1,14 +1,19 @@
 <?php
-    $focusposts = "";
-    $focusfave = "";
-    $focusflwing = "";
-    $focusflwers = "class='focus'";
-    include 'session.php';
-    include '../components/head.php';
-    include '../components/navbar.php';    
-    include '../components/profile.header.php';
-    include '../components/profile.followers.php';
-    include '../components/footer.php';
+//============= PÁGINA DOS SEGUIDORES DO UTILIZADOR ========================
+
+    $focusposts = "";                //Aba dos posts: sem classe   
+    $focusfave = "";                 //Aba dos favorites: sem classe
+    $focusflwing = "";               //Aba dos following: sem classe
+    $focusflwers =  "class='focus'"; //Aba dos followers: acrescentamos a classe ".focus" à respectiva <a> âncora
+    //Resultado: a aba dos followers aparece sublinhada a azul
+
+    //Componentes                                   
+    include 'session.php';                          //Ficheiro PHP com as validações e variáveis de sessão     
+    include '../components/head.php';               //Cabeçalho do HTML:    <doctype>, <head>, <body>, <link>, <styles defer>
+    include '../components/navbar.php';             //Barra de navegação:   <header> <nav>
+    include '../components/profile.header.php';     //Cabeçalho do perfil:  <main>
+    include '../components/profile.followers.php';  //Conteúdo: FOLLOWERS   </main>
+    include '../components/footer.php';             //Roda-pé com links     <footer>
 ?>
 
 <script src="../scripts/posts_dropdown.js"></script>
--- server/following.php
+++ server/following.php
@@ -1,14 +1,19 @@
 <?php
-    $focusposts = "";
-    $focusfave = "";
-    $focusflwing = "class='focus'";
-    $focusflwers = "";
-    include 'session.php';
-    include '../components/head.php';
-    include '../components/navbar.php';    
-    include '../components/profile.header.php';
-    include '../components/profile.following.php';
-    include '../components/footer.php';
+//============= PÁGINA DOS PERFIS QUE O UTILIZADOR SEGUE ========================
+
+    $focusposts = "";                //Aba dos posts: sem classe   
+    $focusfave = "";                 //Aba dos favorites: sem classe
+    $focusflwing = "class='focus'";  //Aba dos following: acrescentamos a classe ".focus" à respectiva <a> âncora
+    $focusflwers =  "";              //Aba dos followers: sem classe
+    //Resultado: a aba dos following aparece sublinhada a azul
+
+    //Componentes  
+    include 'session.php';                          //Ficheiro PHP com as validações e variáveis de sessão   
+    include '../components/head.php';               //Cabeçalho do HTML:    <doctype>, <head>, <body>, <link>, <styles defer>
+    include '../components/navbar.php';             //Barra de navegação:   <header> <nav>
+    include '../components/profile.header.php';     //Cabeçalho do perfil:  <main>
+    include '../components/profile.following.php';  //Conteúdo: FOLLOWING   </main>
+    include '../components/footer.php';             //Roda-pé com links     <footer>
 ?>
 
 <script src="../scripts/posts_dropdown.js"></script>
